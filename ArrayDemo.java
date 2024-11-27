2f3b20711c8d9fbf113170d34474e43383a66b8e
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-11-27 18:07:31 +0530

    ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -8,7 +8,7 @@ class ArrayDemo
         Arr[1] = 20;
         Arr[2] = 30;
         Arr[3] = 40;
-        Arr[4] = 50;
+        Arr[4] = 50; 
         
         System.out.println("Number of elements in array are : "+Arr.length);
 
