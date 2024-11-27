c8987482b59b07b30609981ad57d8526de9b7180
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2024-11-27 18:07:53 +0530

    ArrayDemo1.java

--- ArrayDemo1.java
+++ ArrayDemo1.java
@@ -6,7 +6,7 @@ class ArrayDemo1
         
         int Arr[] = new int[]{10,20,30,40,50};
         
-        System.out.println("Number of elements in array are : "+Arr.length);
+        System.out.println("Number of elements in array are : "+Arr.length);  
 
         System.out.println(Arr[0]);
         System.out.println(Arr[1]);
