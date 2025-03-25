c8f2f34d391aaed7c3c76bf8618101cda11a23ac
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-03-25 09:57:35 +0530

    Multi1.java

--- Multi1.java
+++ Multi1.java
@@ -3,7 +3,7 @@ class Multi1
     public static void main(String arg[])
     {
         String name = Thread.currentThread().getName();
-
+ 
         System.out.println("Name of thread is : "+name);
     }
 } 
