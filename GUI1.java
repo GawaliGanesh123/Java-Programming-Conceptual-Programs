4761aecfd2f7b3eb5d4bab12ebef70ac038325c6
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-04-03 22:45:28 +0530

    GUI1.java

--- GUI1.java
+++ GUI1.java
@@ -4,7 +4,7 @@ class GUI1
 {
     public static void main(String A[])
     {
-        Frame fobj = new Frame("PPA50");
+        Frame fobj = new Frame("PPA50"); 
         fobj.setSize(600,600);
         fobj.setVisible(true);
     }
